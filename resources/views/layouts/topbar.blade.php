c2b60ccc31d3b493c7e9b29932eba7026a88c08a
satya2221 <satyaghifari12@gmail.com>
2023-08-23 21:46:14 +0700

    style[layout login & register]: Ubah layout login dan register

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -166,10 +166,16 @@
         <div class="topbar-divider d-none d-sm-block"></div>
 
         <!-- Nav Item - User Information -->
+        @guest
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        </li>
+        @endguest
+        @auth
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
                 <img class="img-profile rounded-circle"
                     src="img/undraw_profile.svg">
             </a>
@@ -195,7 +201,7 @@
                 </a>
             </div>
         </li>
-
+        @endauth
     </ul>
 
 </nav>
